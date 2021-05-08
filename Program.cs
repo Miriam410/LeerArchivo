707a6723e5fd51446402b31ce00dd3890c2f64e2
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-05-08 00:23:25 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeerArchivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("PlanDeCuentas.txt");
+            Console.WriteLine("El contenido del archivo = ");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+            Console.WriteLine("\t");
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadLine();
+
+        }
+    }
+}
